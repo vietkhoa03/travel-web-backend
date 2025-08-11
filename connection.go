3da767bd3e60138058d59cb37967dbe061a1181d
VK <nguyenvietkhoa003@gmail.com>
2025-08-11 13:48:21 +0700

    connect DB Travel

--- connection.go
+++ connection.go
@@ -5,43 +5,51 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	// Load file .env
 	if err := godotenv.Load(); err != nil {
 		log.Println("No found .env file")
 	}
 
 	uri := os.Getenv("MONGODB_URI")
-
 	if uri == "" {
-    	log.Fatal("MONGODB_URI is not set")
+		log.Fatal("MONGODB_URI is not set")
 	}
 
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
+	// Context với timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
+	// Kết nối MongoDB
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
 	if err != nil {
-		panic(err)
+		log.Fatalf("Connect error: %v", err)
 	}
 
+	// Đảm bảo đóng kết nối khi xong
 	defer func() {
 		if err = client.Disconnect(context.Background()); err != nil {
-			panic(err)
+			log.Fatalf("Disconnect error: %v", err)
 		}
 	}()
 
-	result, err := client.ListDatabaseNames(context.Background(), bson.D{})
-
-	if err != nil {
-		log.Fatal(err)
+	// Ping để kiểm tra kết nối
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatalf("Ping error: %v", err)
 	}
 
-	for _, db := range result {
-		fmt.Println(db)
+	// Lấy DB Travel
+	db := client.Database("Travel")
+
+	// In ra thông báo nếu thành công
+	if db != nil {
+		fmt.Println("connect success")
 	}
 }
